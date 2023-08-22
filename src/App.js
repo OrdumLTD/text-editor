e4ba43161b8afa79eadc3889ea7eb7ada940f2fb
Ivo <jack@Ivos-MBP.lan>
2023-08-22 10:49:01 +0300

    tables can be added and resisized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Editor from "./Editor";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ordum's Text Editor Example</h1>
+      <p>This may look different in the app</p>
+      <Editor />
+    </div>
+  );
+}
